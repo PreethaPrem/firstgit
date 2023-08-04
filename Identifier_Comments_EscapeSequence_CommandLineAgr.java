e00e25ac618391b0c7ad0a751a74a79eeeae9b94
PreethaPrem <136674357+PreethaPrem@users.noreply.github.com>
2023-08-04 05:25:01 +0000

    java

---
+++ Identifier_Comments_EscapeSequence_CommandLineAgr.java
@@ -0,0 +1,38 @@
+
+package Basics;
+
+public class Identifier_Comments_EscapeSequence_CommandLineAgr {
+    public static void main(String[] args){
+        
+        /*Variables/Identifier
+        Conditions
+            1.number's should not used in the staring of the variables
+            2.variables should contain any white space
+            3.should contain any special character 
+        
+        */
+        int name_1=2;//variable
+        System.out.println(name_1);//identifier
+        
+        /*
+        Comments
+           1.single line comment 
+           2.multiline comment 
+           3.java Doc comment = API Purpose + Documentation
+        */
+        
+        System.out.println("Hello\tWorld");
+        System.out.println("Hello  \btWorld");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello\\World\\");
+        System.out.println("Hello\'World\'");
+        System.out.println("Hello\"World\"");
+        System.out.println("Hello\rWorld");
+        System.out.println("Hello\fWorld");
+        
+       
+       
+    } 
+}
+
+
